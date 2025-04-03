4fee0687a36369e95ff5c28e3b67957da0da132c
LLeny <LLeny@users.noreply.github.com>
2025-04-03 08:34:37 +0800

    Fix dependency issue with current versions of libcosmic and cosmic-text

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,5 @@ pub mod options;
 pub mod state;
 pub mod style;
 
+#[cfg(all(feature = "iced", feature = "libcosmic"))]
+compile_error!("feature \"iced\" and feature \"libcosmic\" cannot be enabled at the same time");
